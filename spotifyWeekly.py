a46d60975b1e8fee7843aac6f78b74f4a4aec00c
Matthew Watts <mtthewwatts@gmail.com>
2023-12-17 01:40:58 -0500

    Update to generalize spotifyWeekly.py

--- spotifyWeekly.py
+++ spotifyWeekly.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config['SESSION_COOKIE_NAME'] = 'Spotify Cookie'
 
 # set a random secret key to sign the cookie
-app.secret_key = 'awdoawdiawdaoidhaoiwhawoihdawoihdawo3284qq23039q2293'
+app.secret_key = 'YOUR_SECRET_KEY'
 
 # set the key for the token info in the session dictionary
 TOKEN_INFO = 'token_info' 
@@ -118,8 +118,8 @@ def get_token():
 
 def create_spotify_oauth():
     return SpotifyOAuth(
-        client_id = "6c767823b9884751b130fc11bce31ed9",
-        client_secret = "11b161692d344f5cb42927cfebdd970b",
+        client_id = "YOUR_CLIENT_ID",
+        client_secret = "YOUR_CLIENT_SECRET",
         redirect_uri = url_for('redirect_page', _external = True),
         scope = 'user-library-read playlist-modify-public playlist-modify-private'
         )
